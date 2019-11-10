5151124dce9edf4e14c7874df4eac05f381c1f9b
Collin Cashio <collin.cashio@selu.edu>
2019-11-10 17:53:15 -0600

    big start

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+    </Router>
+  )
+}
